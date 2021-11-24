d5d22b0d2384d9944060751b619cc04d2f605515
karelvanoordt <vanoordtkarel@gmail.com>
2021-11-24 06:15:01 -0500

    Fix linter issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator';
 
 class App extends React.Component {
   constructor(props) {
